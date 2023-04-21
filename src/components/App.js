841b496534ed52a66d140b78ce7e6c147d773eea
Tanay Bhuta <tanaybhuta07@gmail.com>
2023-04-22 02:26:56 +0530

    next commit

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+import Header from "./Header";
+import AddContact from "./AddContact";
+import ContactList from "./ContactList";
+
+function App() {
+//   const contacts = {
+//     {
+      
+
+//     }
+//   }
+  return (
+    <div className="ui container" >
+      <Header />
+      <AddContact/>
+      <ContactList/>
+    </div>
+  );
+}
+
+export default App;
